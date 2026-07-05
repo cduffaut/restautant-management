a5925b550accd273e8f8f822d19ab26ce6f52c1e
cduffaut <cecileduffaut21@gmail.com>
2026-07-05 20:19:43 +0200

    feat(main): initialize all routes

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"restaurant-management/middleware"
+	"restaurant-management/routes"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentification())
+
+	routes.FoodRoutes(router)
+	routes.InvoiceRoutes(router)
+	routes.MenuRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemsRoutes(router)
+	routes.TableRoutes(router)
+
+	router.Run(":" + port)
+}
