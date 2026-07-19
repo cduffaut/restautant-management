6d0e81ac0af661b4d7c46b8e4466af4f49a3af1d
cduffaut <cecileduffaut21@gmail.com>
2026-07-19 13:39:49 +0200

    feat(helpers): create generate all tokens function

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,35 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+	"restaurant-management/helpers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Authentification() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.ValidateToken(clientToken)
+
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf(err)})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("firstname", claims.Firstname)
+		c.Set("lastname", claims.Lastname)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
+	}
 }
